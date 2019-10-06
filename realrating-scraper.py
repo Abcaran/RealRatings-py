57cfb4529979a983bb6d1a2e31adc51cf99b0123
Samir <samirsaadi@gmail.com>
2019-10-05 22:23:45 -0300

    Searching in imdb done!

---
+++ realrating-scraper.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+
+def search_url(url, content):
+    options = Options()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.add_experimental_option("detach", True)
+
+    browser = webdriver.Chrome(options=options)
+
+    browser.get(url)
+    # html = broswer.page_source
+
+    # search_field = browser.find_element_by_xpath("//input[@type='text']")
+    search_field = browser.find_elements_by_id("navbar-query")[0]
+    search_field.send_keys(content, Keys.ENTER)
+    browser.get(browser.current_url)
+    # result_url = browser.find_element_by_xpath(
+    # "//td[@class='result_text']").find_element_by_tag_name("a").click()
+    result_url = browser.find_elements_by_class_name(
+        "result_text")[0].find_element_by_tag_name("a").click()
+    # browser.get(result_url)
+
+    print(result_url)
+
+
+def main():
+    imdb_url = "https://imdb.com/"
+    search_content = "Joker"
+
+    search_url(imdb_url, search_content)
+
+
+main()
